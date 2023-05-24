99d5099bdb678cca26e330a274f134b22c0076ea
Brhwu <106353073+Brhwu@users.noreply.github.com>
2023-05-24 09:46:13 -0700

    added intro scene and removed some redundent code

--- game.js
+++ game.js
@@ -1,3 +1,36 @@
+class intro extends Phaser.Scene {
+    constructor() {
+        super('intro');
+    }
+    preload() {
+        this.load.image("player", 'assets/mc.png')
+        this.load.image("enemy", 'assets/enemy.png')
+        this.load.image("portal", 'assets/portal.png')
+        this.load.image("supplies", 'assets/supplies.png')
+    }
+    create() {
+        this.player = this.physics.add.image(100, 200, 'player');
+        this.sent1 = this.add.text(50, 50, 'This is you.  You are a roamer of the wasteland looking for supplies.');
+        this.sent2 = this.add.text(50, 100, 'When playing use the arrow keys to move.');
+
+        this.sent3 = this.add.text(50, 300, 'This is an image of a zombie, if you encounter one and it touches you, you will die.');
+        this.enemy = this.add.image(100, 400, 'enemy');
+        //this.portal = this.physics.add.sprite(900, 600, 'portal');
+        //this.spawner = this.physics.add.sprite(900, 600, 'spawner');
+        this.sent4 = this.add.text(50, 500, 'These are supplies.  You will have to collect 10 of these before you can leave.');
+        this.supplies = this.physics.add.image(100, 600, 'supplies');
+        this.supplies.setScale(0.5);
+
+        this.sent5 = this.add.text(400, 750, 'This is a portal, you will have to move here to leave.');
+        this.portal = this.physics.add.image(650, 700, 'portal')
+        this.time.delayedCall(6000, () => {
+            this.scene.start('level1');     
+        });
+
+    }
+
+}
+
 class level1 extends Phaser.Scene {
     constructor() {
         super('level1');
@@ -92,6 +125,8 @@ class level1 extends Phaser.Scene {
       
     }
 }
+
+
 
 class level2 extends Phaser.Scene {
     constructor() {
@@ -313,7 +348,7 @@ new Phaser.Game({
     width: 1000, 
     height: 780, 
     backgroundColor: '#3498db', 
-    scene: [level1, level2, level3], 
+    scene: [intro, level1, level2, level3], 
     physics: { default: 'arcade' }, 
     parent: 'game', 
 });
